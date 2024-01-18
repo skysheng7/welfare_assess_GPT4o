1fe8e2e582c5ab26d63bd0d9ca9135a0adb9f376
skysheng7 <82303914+skysheng7@users.noreply.github.com>
2024-01-18 12:40:34 -0800

    update

--- src/pariwise_video_compare.py
+++ src/pariwise_video_compare.py
@@ -126,18 +126,18 @@ max_tokens=1000
 detail_level="low"
 temperature = 0.5
 choosen_quality1 = "good"
-choosen_quality2 = "good"
+choosen_quality2 = "bad"
 full_path = crete_result_path(results_folder, results_file)
 
 # select a good video as example
-video_path1 = select_video_path(i=644, choosen_quality = choosen_quality1, choosen_category = "NA.", bad_videos_by_category=bad_videos_by_category, bad_videos=bad_videos, good_videos=good_videos)
+video_path1 = select_video_path(i=644, choosen_quality = choosen_quality1, choosen_category = "NA", bad_videos_by_category=bad_videos_by_category, bad_videos=bad_videos, good_videos=good_videos)
 extracted_frames1 = extract_frames(video_path1, frames_per_second)
 show_extracted_frames(extracted_frames1)
 print(video_path1)
 
-for i in range(1, 10):
+for i in range(4, 10):
     # select a good video as test
-    video_path2 = select_video_path(i, choosen_quality= choosen_quality2, choosen_category = "NA.", bad_videos_by_category=bad_videos_by_category , bad_videos=bad_videos, good_videos=good_videos)
+    video_path2 = select_video_path(i, choosen_quality= choosen_quality2, choosen_category = "NA", bad_videos_by_category=bad_videos_by_category , bad_videos=bad_videos, good_videos=good_videos)
     extracted_frames2 = extract_frames(video_path2, frames_per_second)
     show_extracted_frames(extracted_frames2)
     print(video_path2)
