6adce9084f9a62901f45b32aeeb62d896a52a3c7
skysheng7 <82303914+skysheng7@users.noreply.github.com>
2024-01-18 14:48:16 -0800

    update

--- src/pairwise_lameness_compare.py
+++ src/pairwise_lameness_compare.py
@@ -39,43 +39,6 @@ for category_videos in bad_videos_by_category.values(): # Shuffle each sublist i
 
 
 ###################################################################################################
-########################## GPT-4V generate description for good & bad video #######################
-###################################################################################################
-# choose model parameters and write prompts
-system_prompt = "You are an experienced expert in animal science focusing on dairy cow behavior and health, with 50 years of experience in observing dairy cow gait and behavior through video. You are expert in assessing the quality of videos to select the ones suitable for lameness assessment. \n "
-user_prompt1 = "Your job is to review 2 cow videos, the first video is a good-quality video, and the second video is a bad-quality video. Create a detailed description for each video focusing only on the movement of the cow (not environment or lighting conditions), focusing on characteristics that help differentiate good-quality videos from bad-quality ones that are independent from the frame number. Create a prompt to ask the GPT-4V model in identify video quality in future tasks to identify bad-quality videos due to cow is running. Make sure to view the series of frames in ascending numerical order, starting to review the frames from the smallest to the largest number, as indicated by the red numbers on the top left corner of each frame. The first video that you will be presented below is a good video: "
-user_prompt2 = "The second video is a bad video because the cow runs through the scene, as shown below:"
-
-max_tokens=1000
-detail_level="low"
-temperature = 0.5
-
-# select a good video
-video_path1 = select_video_path(i=1, choosen_quality = "good", choosen_category = "NA.", bad_videos_by_category=bad_videos_by_category, bad_videos=bad_videos, good_videos=good_videos)
-extracted_frames1 = extract_frames(video_path1, frames_per_second)
-show_extracted_frames(extracted_frames1)
-print(video_path1)
-
-# select a bad video containing running
-video_path2 = select_video_path(3, choosen_quality= "bad", choosen_category = "run", bad_videos_by_category=bad_videos_by_category , bad_videos=bad_videos, good_videos=good_videos)
-extracted_frames2 = extract_frames(video_path2, frames_per_second)
-show_extracted_frames(extracted_frames2)
-print(video_path2)
-
-# extract result answer
-result = compare_2video(client, system_prompt, user_prompt1, user_prompt2, base64_frames1=extracted_frames1, base64_frames2=extracted_frames2, max_tokens=max_tokens, detail_level=detail_level, s=seed, temp=temperature)
-result_content = result.choices[0].message.content
-
-# calculate usage
-output_token = result.usage.completion_tokens
-prompt_tokens = result.usage.prompt_tokens
-output_token_p = output_token_cost(output_token)
-prompt_tokens_p = input_token_cost(prompt_tokens)
-total_cost = round((output_token_p+prompt_tokens_p), 3)
-print(total_cost)
-
-
-###################################################################################################
 ########################### GPT-4V 1-shot learning video quality assessment #######################
 ###################################################################################################
 # choose model parameters and write prompts
@@ -111,40 +74,6 @@ output_token_p = output_token_cost(output_token)
 prompt_tokens_p = input_token_cost(prompt_tokens)
 total_cost = round((output_token_p+prompt_tokens_p), 3)
 print(total_cost)
-
-
-###################################################################################################
-########################### GPT-4V 1-shot learning describe good video only #######################
-###################################################################################################
-# choose model parameters and write prompts
-system_prompt = "You are an experienced expert in animal science focusing on dairy cow behavior and health, with 50 years of experience in observing dairy cow gait and behavior through video. You are expert in assessing the quality of videos to select the ones suitable for accurate lameness assessment. Criteria for good video: Shows a single dairy cow walking smoothly in a fairly straight path with steady pace. The cow should start walking from the side of the screen shaded in green and exit to the opposite side. The presence of a person walking closely behind the cow is acceptable and does not disqualify the video from being considered good. \n "
-user_prompt1 = f"You will be provided with 2 videos, first is an example `good` video to be used as a reference for good quality videos, second is the test video. Your job is to review cow videos (a series of frames) frame by frame, and classify test video as `good` or `bad` based on these criteria. While evaluating the cow's speed, bear in mind that you are reviewing a sequence of frames extracted at {frames_per_second} frames per second, and not the original video. Make sure to view the series of frames in ascending numerical order, starting from the smallest to the largest number, as indicated by the red numbers on the top left corner of each frame.\n "
-user_prompt1 = user_prompt1 + "Essential: Give your assessment with a confidence score from 0-1 and briefly explain your reasoning to clarify your thought process step by step. Take a deep breath before you answer. This task is vital to my career, and I greatly value your thorough analysis. \n Answer format: ```json \n {\n  \"quality\": \"...\",\n \"confidence\": \"...\",\n  \"reason\": \"...\"\n}``` First, here is an example `good` video: "
-user_prompt2 = "Second, here is a test video needs your assessment for video quality:"
-
-max_tokens=1000
-detail_level="low"
-temperature = 0.5
-choosen_quality1 = "good"
-choosen_quality2 = "good"
-full_path = crete_result_path(results_folder, results_file)
-
-# select a good video as example
-video_path1 = select_video_path(i=644, choosen_quality = choosen_quality1, choosen_category = "NA", bad_videos_by_category=bad_videos_by_category, bad_videos=bad_videos, good_videos=good_videos)
-extracted_frames1 = extract_frames(video_path1, frames_per_second)
-show_extracted_frames(extracted_frames1)
-print(video_path1)
-
-for i in range(5, 10):
-    # select a good video as test
-    video_path2 = select_video_path(i, choosen_quality= choosen_quality2, choosen_category = "NA", bad_videos_by_category=bad_videos_by_category , bad_videos=bad_videos, good_videos=good_videos)
-    extracted_frames2 = extract_frames(video_path2, frames_per_second)
-    show_extracted_frames(extracted_frames2)
-    print(video_path2)
-
-    # extract result answer
-    result = compare_2video(client, system_prompt, user_prompt1, user_prompt2, base64_frames1=extracted_frames1, base64_frames2=extracted_frames2, max_tokens=max_tokens, detail_level=detail_level, s=seed, temp=temperature)
-    save_1shot_quality_assess_results_to_csv(full_path, video_path1, video_path2, result, system_prompt, user_prompt1, user_prompt2, choosen_quality1, choosen_quality2, frames_per_second, max_tokens, detail_level, seed, temperature)
 
 
 ###################################################################################################
