9cddfa6fe00ef144092ee40dafaa5e33e9a650e9
skysheng7 <82303914+skysheng7@users.noreply.github.com>
2024-01-18 00:50:08 -0800

    good description of the video only

--- src/pariwise_video_compare.py
+++ src/pariwise_video_compare.py
@@ -15,7 +15,7 @@ import random
 
 # define result directory
 results_folder = '../results'
-results_file = 'pairwise_video_quality_description.csv' # store the results in a csv file
+results_file = '1shot_good_description_quality_assess.csv' # store the results in a csv file
 
 # connect to OpenAI API
 load_dotenv()  # This loads the variables (API key) from .env
@@ -111,6 +111,45 @@ output_token_p = output_token_cost(output_token)
 prompt_tokens_p = input_token_cost(prompt_tokens)
 total_cost = round((output_token_p+prompt_tokens_p), 3)
 print(total_cost)
+
+
+###################################################################################################
+########################### GPT-4V 1-shot learning describe good video only #######################
+###################################################################################################
+# choose model parameters and write prompts
+system_prompt = "You are an experienced expert in animal science focusing on dairy cow behavior and health, with 50 years of experience in observing dairy cow gait and behavior through video. You are expert in assessing the quality of videos to select the ones suitable for accurate lameness assessment. Criteria for good video: Shows a single dairy cow walking smoothly in a fairly straight path with steady pace. The cow should start walking from the side of the screen shaded in green and exit to the opposite side. The presence of a person walking closely behind the cow is acceptable and does not disqualify the video from being considered good. \n "
+user_prompt1 = f"You will be provided with 2 videos, first is an example `good` video to be used as a reference for good quality videos, second is the test video. Your job is to review cow videos (a series of frames) frame by frame, and classify test video as `good` or `bad` based on these criteria. While evaluating the cow's speed, bear in mind that you are reviewing a sequence of frames extracted at {frames_per_second} frames per second, and not the original video. Make sure to view the series of frames in ascending numerical order, starting from the smallest to the largest number, as indicated by the red numbers on the top left corner of each frame.\n "
+user_prompt1 = user_prompt1 + "Essential: Give your assessment with a confidence score from 0-1 and briefly explain your reasoning to clarify your thought process step by step. Take a deep breath before you answer. This task is vital to my career, and I greatly value your thorough analysis. \n Answer format: ```json \n {\n  \"quality\": \"...\",\n \"confidence\": \"...\",\n  \"reason\": \"...\"\n}``` First, here is an example `good` video: "
+user_prompt2 = "Second, here is a test video needs your assessment for video quality:"
+
+max_tokens=1000
+detail_level="low"
+temperature = 0.5
+
+# select a good video as example
+video_path1 = select_video_path(i=644, choosen_quality = "good", choosen_category = "NA.", bad_videos_by_category=bad_videos_by_category, bad_videos=bad_videos, good_videos=good_videos)
+extracted_frames1 = extract_frames(video_path1, frames_per_second)
+show_extracted_frames(extracted_frames1)
+print(video_path1)
+
+# select a good video as test
+video_path2 = select_video_path(i=9, choosen_quality= "good", choosen_category = "NA.", bad_videos_by_category=bad_videos_by_category , bad_videos=bad_videos, good_videos=good_videos)
+extracted_frames2 = extract_frames(video_path2, frames_per_second)
+show_extracted_frames(extracted_frames2)
+print(video_path2)
+
+# extract result answer
+result = compare_2video(client, system_prompt, user_prompt1, user_prompt2, base64_frames1=extracted_frames1, base64_frames2=extracted_frames2, max_tokens=max_tokens, detail_level=detail_level, s=seed, temp=temperature)
+result_content = result.choices[0].message.content
+
+# calculate usage
+output_token = result.usage.completion_tokens
+prompt_tokens = result.usage.prompt_tokens
+output_token_p = output_token_cost(output_token)
+prompt_tokens_p = input_token_cost(prompt_tokens)
+total_cost = round((output_token_p+prompt_tokens_p), 3)
+print(total_cost)
+
 
 ###################################################################################################
 ####################################### pairwise lameness comparison ##############################
