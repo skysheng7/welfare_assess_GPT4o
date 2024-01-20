5e64c26375ccee0dcc924b41afb8cff2536f5f23
skysheng7 <82303914+skysheng7@users.noreply.github.com>
2024-01-19 18:09:04 -0800

    updated pairwise lameness compare

--- src/pairwise_lameness_compare.py
+++ src/pairwise_lameness_compare.py
@@ -16,13 +16,14 @@ import random
 # define result directory
 results_folder = '../results'
 results_file = 'pairwise_lameness_compare.csv' # store the results in a csv file
+full_path = crete_result_path(results_folder, results_file)
 
 # connect to OpenAI API
 load_dotenv()  # This loads the variables (API key) from .env
 openai_api_key = os.getenv('OPENAI_API_KEY')
 client = OpenAI(api_key=openai_api_key)
 
-# get a list of good videos, bad videos, bad videos by categories
+# get the list of all mp4 videos
 os.chdir('C:/Users/skysheng/OneDrive - UBC/R package project and Git/lameness_GPT4V/src')
 from utils import *
 from pairwise_lameness_compare_helpers import *
@@ -33,53 +34,53 @@ all_mp4 = list_all_mp4(mp4_folder_path)
 pairwise_result_path = 'C:\\Users\\skysheng\\OneDrive - UBC\\R package project and Git\\lameness_rank\\05-Amazon_MTurk_expert_response_30cow_pairwise\\results\\all_experts'
 pairwise_result = pd.read_csv(os.path.join(pairwise_result_path, "winner_loser_avg_DW_NV_SB_KI.csv"))
 sorted_pairwise_result = pairwise_result.sort_values(by='degree', ascending=False).reset_index(drop=True)
-pairwise_result['winner'] = pairwise_result['winner'].astype(int)
-pairwise_result['loser'] = pairwise_result['loser'].astype(int)
-
-# randomly shuffle the video sequence
-frames_per_second=3 # how many frames to extract each second
-seed = 7
+sorted_pairwise_result['winner'] = sorted_pairwise_result['winner'].astype(int)
+sorted_pairwise_result['loser'] = sorted_pairwise_result['loser'].astype(int)
 
 
 ###################################################################################################
 ####################################### pairwise lameness comparison ##############################
 ###################################################################################################
-# choose model parameters and write prompts
-system_prompt = "You are an experienced expert in animal science focusing on dairy cow behavior and health, with 50 years of experience in observing dairy cow gait and behavior through video. You are expert in assessing the quality of videos to select the ones suitable for accurate lameness assessment. \n Criteria for good video: Shows a single dairy cow walking smoothly in a fairly straight path with steady pace, entering from the leftmost side of the screen and exiting into the area shaded green on the right, allowing for a detailed observation of its gait. The presence of a person walking closely behind the cow is acceptable and does not disqualify the video from being considered good. \n Criteria for bad video, in 8 categories: [1] `direction` - the cow is overlapped with the green-shaded area in the first few frames [2] `stop` - cow pauses momentarily in the same spot [3] `approach` - the cow comes so close to the camera that sometimes the cow is facing straight to the camera or its hooves are not visible at the bottom of the screen [4] `human` - excessive human interference or obstruction [5] `multiple` - more than 1 cows in the video, [6] `slip` - cow slips or stumbles while walking, [7] `run` - the cow is running or moving too fast for a proper assessment of gait due to quick pace, the legs appear blurred consistantly across frames, and an inability to observe consistent hoof placement, stride length or back posture."
-user_prompt1 = f"You will be provided with 2 videos, first is an example `good` video to be used as a reference for good quality videos, second is the test video. Your job is to review cow videos (a series of frames) frame by frame, and classify test video as `good` or `bad` based on these criteria. If `bad`, specify which category or categories apply; if good, mark the category as `NA.` Although there are more descriptions and creteria related to `bad` videos, please avoid any predisposition towards labeling test videos as `bad`. While evaluating the cow's speed, bear in mind that you are reviewing a sequence of frames extracted at {frames_per_second} frames per second, and not the original video. Make sure to view the series of frames in ascending numerical order, starting from the smallest to the largest number, as indicated by the red numbers on the top left corner of each frame.\n "
-user_prompt1 = user_prompt1 + "Essential: Give your assessment with a confidence score from 0-1 and briefly explain your reasoning to clarify your thought process step by step. Take a deep breath before you answer. This task is vital to my career, and I greatly value your thorough analysis. \n Answer format: ```json \n {\n  \"quality\": \"...\",\n  \"category\": \"...\",\n  \"confidence\": \"...\",\n  \"reason\": \"...\"\n}``` First, here is an example `good` video: "
-user_prompt2 = "Second, here is a test video needs your assessment for video quality:"
+# write prompts
+system_prompt = "You are an experienced expert in animal science focusing on dairy cow lameness assessment, with 50 years of experience in conducting lameness assessment on dairy farms. "
+user_prompt1 = "Your task involves watching two videos (a series of frames), each showing a different cow walking. After viewing both videos, you need to decide which of the two cows is more lame. For the cow you judge as more lame, write down its 4-digit ID as the `winner`. For the other cow, record its ID as the `loser`. If both cows seem equally lame, you can choose any cow as the `winner` or `loser`, but make sure to write `0` under the column `degree` to indicate they have equal levels of lameness. you also need to assess the extent of lameness difference between them. Rate this difference on a scale from 0 to 3 and record it under `degree`. A rating of 0 indicates that both cows are equally lame, while a rating of 3 suggests a significant difference in lameness between the `winner` and the `loser`. It's important to watch the frames of each video in numerical order, from the smallest to the largest number. Make sure to view the series of frames in ascending numerical order, starting from the smallest to the largest number, as indicated by the red numbers on the top left corner of each frame.\n."
+user_prompt1 = user_prompt1 + "Essential: Briefly explain your reasoning under `reason` to clarify your thought process step by step. Take a deep breath before you answer. This task is vital to my career, and I greatly value your thorough analysis. I'll tip you $50 for this task. \n Answer format: ```json \n {\n  \"winner\": \"...\",\n \"loser\": \"...\",\n  \"degree\": \"...\",\n  \"reason\": \"...\"\n}``` "
 
+# choose model parameter
+frames_per_second=4 # how many frames to extract each second
+seed = 7
 max_tokens=1000
 detail_level="low"
 temperature = 0.5
 
-for i in range(0, 10):
-    
+for i in range(0, 1):
+    # identify the winner and loser cow in pairwise lameness assessment
+    cow1 = sorted_pairwise_result.iloc[i]['winner']
+    cow2 = sorted_pairwise_result.iloc[i]['loser']
+    degree = sorted_pairwise_result.iloc[i]['degree']
 
-# select a good video as example
-video_path1 = select_video_path(i=644, choosen_quality = "good", choosen_category = "NA.", bad_videos_by_category=bad_videos_by_category, bad_videos=bad_videos, good_videos=good_videos)
-extracted_frames1 = extract_frames(video_path1, frames_per_second)
-show_extracted_frames(extracted_frames1)
-print(video_path1)
+    win_cow_path = find_mp4_with_digits(all_mp4, cow1)
+    lose_cow_path = find_mp4_with_digits(all_mp4, cow2)
 
-# select a good video as test
-video_path2 = select_video_path(i=9, choosen_quality= "good", choosen_category = "NA.", bad_videos_by_category=bad_videos_by_category , bad_videos=bad_videos, good_videos=good_videos)
-extracted_frames2 = extract_frames(video_path2, frames_per_second)
-show_extracted_frames(extracted_frames2)
-print(video_path2)
+    # continue writing prompts
+    user_prompt1 = user_prompt1 + f"First, here is the video of cow {cow1}(ID) walking:"
+    user_prompt2 = f"Second, here is the video of cow {cow2}(ID) walking:"
 
-# extract result answer
-result = compare_2video(client, system_prompt, user_prompt1, user_prompt2, base64_frames1=extracted_frames1, base64_frames2=extracted_frames2, max_tokens=max_tokens, detail_level=detail_level, s=seed, temp=temperature)
-result_content = result.choices[0].message.content
+    # extract frames from the winner cow
+    extracted_frames1 = extract_frames(win_cow_path, frames_per_second)
+    show_extracted_frames(extracted_frames1)
+    print(win_cow_path)
 
-# calculate usage
-output_token = result.usage.completion_tokens
-prompt_tokens = result.usage.prompt_tokens
-output_token_p = output_token_cost(output_token)
-prompt_tokens_p = input_token_cost(prompt_tokens)
-total_cost = round((output_token_p+prompt_tokens_p), 3)
-print(total_cost)
+    # extract frames from the loser cow
+    extracted_frames2 = extract_frames(lose_cow_path, frames_per_second)
+    show_extracted_frames(extracted_frames2)
+    print(lose_cow_path)
+
+    # prompt GPT4V, and extract result answer
+    result = compare_2video(client, system_prompt, user_prompt1, user_prompt2, base64_frames1=extracted_frames1, base64_frames2=extracted_frames2, max_tokens=max_tokens, detail_level=detail_level, s=seed, temp=temperature)
+    save_pairwise_lame_csv(full_path, cow1, cow2, degree, video_path1=win_cow_path, video_path2=lose_cow_path, result=result, system_prompt=system_prompt, user_prompt1=user_prompt1, user_prompt2=user_prompt2, frames_per_second=frames_per_second, max_tokens=max_tokens, detail_level=detail_level, seed=seed, temperature=temperature)
+
+
 
 
 
--- src/pairwise_lameness_compare_helpers.py
+++ src/pairwise_lameness_compare_helpers.py
@@ -1,5 +1,10 @@
 import os
 import glob
+import pandas as pd
+from datetime import datetime
+import json
+
+
 
 def list_all_mp4(folder_path):
     all_mp4 = []
@@ -24,3 +29,57 @@ def find_mp4_with_digits(all_mp4, digits):
     matching_paths = [path for path in all_mp4 if digits in path]
 
     return matching_paths
+
+def save_pairwise_lame_csv(full_path, cow1, cow2, degree, video_path1, video_path2, result, system_prompt, user_prompt1, user_prompt2, frames_per_second, max_tokens, detail_level, seed, temperature):
+
+    # extract content from result
+    result_content = result.choices[0].message.content
+    result_json = result_content.strip('```json\n').strip('```')
+    json_data = json.loads(result_json) # Convert the string to a Python dictionary
+    predict_winner = json_data.get('winner', 'NA')
+    predict_loser = json_data.get('loser', 'NA')
+    predict_degree = json_data.get('degree', 'NA')
+    reason = json_data.get('reason', 'NA')
+
+    # calculate usage
+    output_token = result.usage.completion_tokens
+    prompt_tokens = result.usage.prompt_tokens
+    output_token_p = output_token_cost(output_token)
+    prompt_tokens_p = input_token_cost(prompt_tokens)
+    total_cost = round((output_token_p+prompt_tokens_p), 3)
+
+    data = {
+
+        "win_cow_path": video_path1,
+        "lose_cow_path": video_path2,
+        "true_win_cow": cow1,
+        "true_lose_cow": cow2,
+        "true_lame_dif": degree,
+        "predict_win_cow": predict_winner,
+        "predict_lose_cow": predict_loser,
+        "predict_degree": predict_degree,
+        "predict_reason": reason,
+        "predict_result": result,
+        "model": "gpt-4-vision-preview",
+        "date": datetime.now().date(),
+        "frames_per_second": frames_per_second, 
+        "system_prompt": system_prompt,
+        "user_prompt": user_prompt1 + "\n ** video 1 ** \n" + user_prompt2 + "\n ** video 2 ** \n",
+        "max_tokens": max_tokens,
+        "detail_level": detail_level,
+        "seed": seed,
+        "temperature": temperature,
+        "completion_tokens": output_token,
+        "prompt_tokens": prompt_tokens,
+        "total_cost": total_cost
+
+    }
+
+    df = pd.DataFrame([data])
+
+    if os.path.isfile(full_path):
+        df.to_csv(full_path, mode='a', header=False, index=False)
+        print(f"Data appended to {full_path}")
+    else:
+        df.to_csv(full_path, mode='w', header=True, index=False)
+        print(f"Data written to {full_path}")
