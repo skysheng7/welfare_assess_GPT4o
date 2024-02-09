59c023f0950604f5998465975d58eb63f4d7885b
skysheng7 <82303914+skysheng7@users.noreply.github.com>
2024-02-08 23:54:32 -0800

    all welfare assessment result 0-shot finish

--- src/welfare_assess.py
+++ src/welfare_assess.py
@@ -62,14 +62,14 @@ test_image_lead = "\n The following image requires your assessment of the cow's 
 user_prompt2 = task + performance_emotion_boost + answer_format + test_image_lead
 
 # prompt GPT-4V
-start_index = 1
+start_index = 0
 end_index = len(test_files)
 test_images_in_range(results_path, start_index, end_index, client, system_prompt, user_prompt1, user_prompt2, train_images, test_images, test_files, detail_level, max_tokens, s=seed, temp=temperature)
 
 
 
 ###################################################################################################
-############################### welfare assessment: hairless ########################
+############################### welfare assessment: hairless ######################################
 ###################################################################################################
 # set input and output dir
 root_folder_path = 'C:/Users/skysheng/OneDrive - UBC/University of British Columbia/Other projects/welfare_assessment_GPT4V/data_official/Hairless'
@@ -90,10 +90,10 @@ test_images = [convert_jpg_to_base64(os.path.join(test, f)) for f in test_files]
 # generate prompts
 system_prompt = "You are an experienced expert in animal welfare science focusing on dairy cow behavior and health, with 20 years of experience in conducting farm audit for welfare assessment. \n "
 user_prompt1 = "Below are images containing text descriptions and criteria for assessing the presence of hairless patches on dairy cows. Please read these text descriptions and examples in the images. Creteria and example images: \n "
-task = "Your task involves evaluating if there are hairless patches on the dairy cow shown in the subsequent image, based on the previously provided criteria and examples. Please record your response under \"assessment_result\": if there are hairless patches, enter \`hairless\`; if no hairless patches are detected, enter \`nothairless\`. \n "
+task = "Your task involves evaluating if there are hairless patches on the dairy cow shown in the subsequent image, based on the previously provided criteria and examples. Please record your response under \"assessment_result\": if there are hairless patches, enter \"hairless\"; if no hairless patches are detected, enter \"nothairless\". \n "
 performance_emotion_boost ="\n Give your assessment with a confidence score and briefly explain your reasoning to clarify your thought process step by step. Take a deep breath before you answer. This task is vital to my career, and I greatly value your thorough analysis. \n"
 answer_format = "\n Answer format: ```json \n {\n  \"assessment_result\": \"...\",\n  \"confidence\": \"...\",\n  \"reason\": \"...\"}``` \n"
-test_image_lead = "\n The following image requires your assessment of the cow's integument alterations: \n"
+test_image_lead = "\n The following image requires your assessment: \n"
 user_prompt2 = task + performance_emotion_boost + answer_format + test_image_lead
 
 # prompt GPT-4V
@@ -102,15 +102,15 @@ end_index = len(test_images)
 test_images_in_range(results_path, start_index, end_index, client, system_prompt, user_prompt1, user_prompt2, train_images, test_images, test_files, detail_level, max_tokens, s=seed, temp=temperature)
 
 
-###################################################################################################
-####################### welfare assessment: udder, hindquarter, hindleg ###########################
-###################################################################################################
 
+###################################################################################################
+############################### welfare assessment: Lesions #######################################
+###################################################################################################
 # set input and output dir
-root_folder_path = 'C:/Users/skysheng/OneDrive - UBC/University of British Columbia/Other projects/welfare_assessment_GPT4V/data_official/Hairless'
+root_folder_path = 'C:/Users/skysheng/OneDrive - UBC/University of British Columbia/Other projects/welfare_assessment_GPT4V/data_official/Lesions'
 train = os.path.join(root_folder_path, "train")
 test = os.path.join(root_folder_path, "test")
-results_file = 'welfare_assess_udder_hindquarter_hindleg.csv' # store the results in a csv file
+results_file = 'welfare_assess_lesions_test.csv' # store the results in a csv file
 results_path = crete_result_path(results_folder, results_file)
 
 # train image examples: Get all PNG files in the train folder and sort them
@@ -124,11 +124,81 @@ test_images = [convert_jpg_to_base64(os.path.join(test, f)) for f in test_files]
 
 # generate prompts
 system_prompt = "You are an experienced expert in animal welfare science focusing on dairy cow behavior and health, with 20 years of experience in conducting farm audit for welfare assessment. \n "
-user_prompt1 = "Below are images containing text descriptions and criteria for assessing cleanliness of dairy cows' [1] udder, [2]hindquarter, and [3]hindleg. Please read these examples in the images. Creteria and example images: \n "
-task = "Your task involves evaluating the cleanliness of dairy cows' [1]udder, [2]hindquarter, and [3]hindleg shown in the subsequent image, based on the previously provided criteria and examples.  `udder`: Enter `0` if no dirt or minor splashing; Enter `2` if there are distinct plaques of dirt on udder or any dirt on and around the teats. `hindquarter`: Enter `0` if no dirt or minor splashing; Enter `2` if there are separate or continuous plaques of dirt. `hindleg`: Enter `0` if no dirt or minor splashing; Enter `2` if there are separate or continuous plaques of dirt above the coronary band. If you can not assess due to image angle, enter `NA`\n "
+user_prompt1 = "Below are images containing text descriptions and criteria for assessing the presence of lesions on dairy cows. Please read these text descriptions and examples in the images. Creteria and example images: \n "
+task = "Your task involves evaluating if there are lesions on the dairy cow shown in the subsequent image, based on the previously provided criteria and examples. Please record your response under \"assessment_result\": if there are lesions, enter \"lesion\"; if no lesion is detected, enter \"nolesion\". \n "
 performance_emotion_boost ="\n Give your assessment with a confidence score and briefly explain your reasoning to clarify your thought process step by step. Take a deep breath before you answer. This task is vital to my career, and I greatly value your thorough analysis. \n"
-answer_format = "\n Answer format: ```json \n {\n  \"assessment_result\": \"...\",\n \"confidence\": \"...\",\n  \"reason\": \"...\"}``` \n"
-test_image_lead = "\n The following image requires your assessment of the cow's cleanliness of udder, hindquarter, and hindleg: \n"
+answer_format = "\n Answer format: ```json \n {\n  \"assessment_result\": \"...\",\n  \"confidence\": \"...\",\n  \"reason\": \"...\"}``` \n"
+test_image_lead = "\n The following image requires your assessment: \n"
+user_prompt2 = task + performance_emotion_boost + answer_format + test_image_lead
+
+# prompt GPT-4V
+start_index = 0
+end_index = len(test_images)
+test_images_in_range(results_path, start_index, end_index, client, system_prompt, user_prompt1, user_prompt2, train_images, test_images, test_files, detail_level, max_tokens, s=seed, temp=temperature)
+
+
+
+###################################################################################################
+############################### welfare assessment: Swelling ######################################
+###################################################################################################
+# set input and output dir
+root_folder_path = 'C:/Users/skysheng/OneDrive - UBC/University of British Columbia/Other projects/welfare_assessment_GPT4V/data_official/Swelling'
+train = os.path.join(root_folder_path, "train")
+test = os.path.join(root_folder_path, "test")
+results_file = 'welfare_assess_swelling_test.csv' # store the results in a csv file
+results_path = crete_result_path(results_folder, results_file)
+
+# train image examples: Get all PNG files in the train folder and sort them
+train_files = [f for f in os.listdir(train) if f.lower().endswith('.png')]
+train_files.sort()
+train_images = [convert_to_jpeg_base64(os.path.join(train, f)) for f in train_files] # Convert PNG images to JPEG and then to Base64
+
+# test image examples: 
+test_files = [f for f in os.listdir(test) if f.lower().endswith('.jpg')]
+test_images = [convert_jpg_to_base64(os.path.join(test, f)) for f in test_files]
+
+# generate prompts
+system_prompt = "You are an experienced expert in animal welfare science focusing on dairy cow behavior and health, with 20 years of experience in conducting farm audit for welfare assessment. \n "
+user_prompt1 = "Below are images containing text descriptions and criteria for assessing the presence of swelling on the body of dairy cows. Please read these text descriptions and examples in the images. Creteria and example images: \n "
+task = "Your task involves evaluating if there are swelling on the dairy cow shown in the subsequent image, based on the previously provided criteria and examples. Please record your response under \"assessment_result\": if there is swelling, enter \"swelling\"; if no swelling is detected, enter \"noswelling\". \n "
+performance_emotion_boost ="\n Give your assessment with a confidence score and briefly explain your reasoning to clarify your thought process step by step. Take a deep breath before you answer. This task is vital to my career, and I greatly value your thorough analysis. \n"
+answer_format = "\n Answer format: ```json \n {\n  \"assessment_result\": \"...\",\n  \"confidence\": \"...\",\n  \"reason\": \"...\"}``` \n"
+test_image_lead = "\n The following image requires your assessment: \n"
+user_prompt2 = task + performance_emotion_boost + answer_format + test_image_lead
+
+# prompt GPT-4V
+start_index = 0
+end_index = len(test_images)
+test_images_in_range(results_path, start_index, end_index, client, system_prompt, user_prompt1, user_prompt2, train_images, test_images, test_files, detail_level, max_tokens, s=seed, temp=temperature)
+
+
+
+###################################################################################################
+############################## welfare assessment: Hindleg_cleanliness ############################
+###################################################################################################
+# set input and output dir
+root_folder_path = 'C:/Users/skysheng/OneDrive - UBC/University of British Columbia/Other projects/welfare_assessment_GPT4V/data_official/Hindleg_cleanliness'
+train = os.path.join(root_folder_path, "train")
+test = os.path.join(root_folder_path, "test")
+results_file = 'welfare_assess_Hindleg_cleanliness_test.csv' # store the results in a csv file
+results_path = crete_result_path(results_folder, results_file)
+
+# train image examples: Get all PNG files in the train folder and sort them
+train_files = [f for f in os.listdir(train) if f.lower().endswith('.png')]
+train_files.sort()
+train_images = [convert_to_jpeg_base64(os.path.join(train, f)) for f in train_files] # Convert PNG images to JPEG and then to Base64
+
+# test image examples: 
+test_files = [f for f in os.listdir(test) if f.lower().endswith('.jpg')]
+test_images = [convert_jpg_to_base64(os.path.join(test, f)) for f in test_files]
+
+# generate prompts
+system_prompt = "You are an experienced expert in animal welfare science focusing on dairy cow behavior and health, with 20 years of experience in conducting farm audit for welfare assessment. \n "
+user_prompt1 = "Below are images containing text descriptions and criteria for assessing the cleanliness of dairy cows. Please read these examples in the images. Creteria and example images: \n "
+task = "Your task involves evaluating the cleanliness of the hindleg of dairy cow shown in the subsequent image, and record your score under \"assessment_result\", based on the previously provided criteria and examples. 0: clean; 2: dirty.\n "
+performance_emotion_boost ="\n Give your assessment with a confidence score (low, medium or high) and briefly explain your reasoning to clarify your thought process step by step. Take a deep breath before you answer. This task is vital to my career, and I greatly value your thorough analysis. \n"
+answer_format = "\n Answer format: ```json \n {\n  \"assessment_result\": \"...\",\n  \"confidence\": \"...\",\n  \"reason\": \"...\"}``` \n"
+test_image_lead = "\n The following image requires your assessment: \n"
 user_prompt2 = task + performance_emotion_boost + answer_format + test_image_lead
 
 # prompt GPT-4V
@@ -140,5 +210,70 @@ test_images_in_range(results_path, start_index, end_index, client, system_prompt
 
 
 
+###################################################################################################
+########################## welfare assessment: Hindquarter_cleanliness ############################
+###################################################################################################
+# set input and output dir
+root_folder_path = 'C:/Users/skysheng/OneDrive - UBC/University of British Columbia/Other projects/welfare_assessment_GPT4V/data_official/Hindquarter_cleanliness'
+train = os.path.join(root_folder_path, "train")
+test = os.path.join(root_folder_path, "test")
+results_file = 'welfare_assess_Hindquarter_cleanliness_test.csv' # store the results in a csv file
+results_path = crete_result_path(results_folder, results_file)
 
+# train image examples: Get all PNG files in the train folder and sort them
+train_files = [f for f in os.listdir(train) if f.lower().endswith('.png')]
+train_files.sort()
+train_images = [convert_to_jpeg_base64(os.path.join(train, f)) for f in train_files] # Convert PNG images to JPEG and then to Base64
+
+# test image examples: 
+test_files = [f for f in os.listdir(test) if f.lower().endswith('.jpg')]
+test_images = [convert_jpg_to_base64(os.path.join(test, f)) for f in test_files]
+
+# generate prompts
+system_prompt = "You are an experienced expert in animal welfare science focusing on dairy cow behavior and health, with 20 years of experience in conducting farm audit for welfare assessment. \n "
+user_prompt1 = "Below are images containing text descriptions and criteria for assessing the cleanliness of dairy cows. Please read these examples in the images. Creteria and example images: \n "
+task = "Your task involves evaluating the cleanliness of the hindquarter of dairy cow shown in the subsequent image, and record your score under \"assessment_result\", based on the previously provided criteria and examples. 0: clean; 2: dirty.\n "
+performance_emotion_boost ="\n Give your assessment with a confidence score (low, medium or high) and briefly explain your reasoning to clarify your thought process step by step. Take a deep breath before you answer. This task is vital to my career, and I greatly value your thorough analysis. \n"
+answer_format = "\n Answer format: ```json \n {\n  \"assessment_result\": \"...\",\n  \"confidence\": \"...\",\n  \"reason\": \"...\"}``` \n"
+test_image_lead = "\n The following image requires your assessment: \n"
+user_prompt2 = task + performance_emotion_boost + answer_format + test_image_lead
+
+# prompt GPT-4V
+start_index = 0
+end_index = len(test_files)
+test_images_in_range(results_path, start_index, end_index, client, system_prompt, user_prompt1, user_prompt2, train_images, test_images, test_files, detail_level, max_tokens, s=seed, temp=temperature)
+
+
+###################################################################################################
+############################ welfare assessment: Udder_cleanliness ################################
+###################################################################################################
+# set input and output dir
+root_folder_path = 'C:/Users/skysheng/OneDrive - UBC/University of British Columbia/Other projects/welfare_assessment_GPT4V/data_official/Udder_cleanliness'
+train = os.path.join(root_folder_path, "train")
+test = os.path.join(root_folder_path, "test")
+results_file = 'welfare_assess_Udder_cleanliness_test.csv' # store the results in a csv file
+results_path = crete_result_path(results_folder, results_file)
+
+# train image examples: Get all PNG files in the train folder and sort them
+train_files = [f for f in os.listdir(train) if f.lower().endswith('.png')]
+train_files.sort()
+train_images = [convert_to_jpeg_base64(os.path.join(train, f)) for f in train_files] # Convert PNG images to JPEG and then to Base64
+
+# test image examples: 
+test_files = [f for f in os.listdir(test) if f.lower().endswith('.jpg')]
+test_images = [convert_jpg_to_base64(os.path.join(test, f)) for f in test_files]
+
+# generate prompts
+system_prompt = "You are an experienced expert in animal welfare science focusing on dairy cow behavior and health, with 20 years of experience in conducting farm audit for welfare assessment. \n "
+user_prompt1 = "Below are images containing text descriptions and criteria for assessing the cleanliness of dairy cows. Please read these examples in the images. Creteria and example images: \n "
+task = "Your task involves evaluating the cleanliness of the udder of dairy cow shown in the subsequent image, and record your score under \"assessment_result\", based on the previously provided criteria and examples. 0: clean; 2: dirty.\n "
+performance_emotion_boost ="\n Give your assessment with a confidence score (low, medium or high) and briefly explain your reasoning to clarify your thought process step by step. Take a deep breath before you answer. This task is vital to my career, and I greatly value your thorough analysis. \n"
+answer_format = "\n Answer format: ```json \n {\n  \"assessment_result\": \"...\",\n  \"confidence\": \"...\",\n  \"reason\": \"...\"}``` \n"
+test_image_lead = "\n The following image requires your assessment: \n"
+user_prompt2 = task + performance_emotion_boost + answer_format + test_image_lead
+
+# prompt GPT-4V
+start_index = 0
+end_index = len(test_files)
+test_images_in_range(results_path, start_index, end_index, client, system_prompt, user_prompt1, user_prompt2, train_images, test_images, test_files, detail_level, max_tokens, s=seed, temp=temperature)
 
