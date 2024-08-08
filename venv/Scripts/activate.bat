f7f7d7cf8e728302766e5e275b732a1196892c8c
skysheng7 <82303914+skysheng7@users.noreply.github.com>
2024-08-07 18:01:27 -0700

    update

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\skysheng\OneDrive - UBC\R package project and Git\lameness_GPT4V\venv
+set VIRTUAL_ENV=C:\Users\skysheng\Desktop\lameness_GPT4V\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
