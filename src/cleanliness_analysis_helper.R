5d1fe4b29a735dc4996beee70911d3cb34178fda
skysheng7 <82303914+skysheng7@users.noreply.github.com>
2024-08-07 19:42:03 -0700

    finalized

--- src/cleanliness_analysis_helper.R
+++ src/cleanliness_analysis_helper.R
@@ -18,7 +18,7 @@ summarize_scores <- function(data) {
     # Calculate the accuracy percentage
     accurate_predictions <- sum(subset_data$predict_score == subset_data$true_score, na.rm = TRUE)
     total_predictions <- nrow(subset_data)
-    accurate_predict_pct <- (accurate_predictions / total_predictions) * 100
+    accurate_predict_pct <- (accurate_predictions / total_predictions) 
     
     # Combine the current row with frequencies and accuracy percentage into a new data frame
     current_result <- cbind(unique_images[i, ], predict_score_0_freq = score_0_freq, predict_score_2_freq = score_2_freq, accurate_predict_pct = accurate_predict_pct)
@@ -30,4 +30,63 @@ summarize_scores <- function(data) {
   # Combine all individual data frames into one data frame
   final_results <- do.call(rbind, results_list)
   return(final_results)
+}
+
+weighted_precision_recall <- function(data) {
+  TP <- sum(data[which(data$true_score == 2), "accurate_predict_pct"])
+  FP <- sum(data[which(data$true_score == 0), "inaccurate_pct"])
+  FN <- sum(data[which(data$true_score == 2), "inaccurate_pct"])
+  
+  precision <- TP / (TP + FP)
+  recall <- TP / (TP + FN)
+  
+  return(list(precision = precision, recall = recall))
+}
+
+calculate_metrics <- function(data) {
+  unique_combinations <- unique(data[c("assess_area", "treatment")])
+  metrics_df <- data.frame(assess_area=character(), treatment=character(), accuracy=numeric(), precision=numeric(), recall=numeric(), stringsAsFactors = FALSE)
+  
+  for (i in seq(nrow(unique_combinations))) {
+    subset_data <- data[data$assess_area == unique_combinations$assess_area[i] & data$treatment == unique_combinations$treatment[i],]
+    summary_subset <- summarize_scores(subset_data)
+    summary_subset$inaccurate_pct <- 1 - summary_subset$accurate_predict_pct
+    
+    # Calculate accuracy, precision, and recall
+    accuracy <- mean(summary_subset$accurate_predict_pct)
+    prec_rec_metrics <- weighted_precision_recall(summary_subset)
+    
+    # Append to the dataframe and ensure types are numeric
+    metrics_df[nrow(metrics_df) + 1, ] <- c(unique_combinations$assess_area[i], unique_combinations$treatment[i], as.numeric(accuracy), as.numeric(prec_rec_metrics$precision), as.numeric(prec_rec_metrics$recall))
+  }
+  
+  return(metrics_df)
+}
+
+plot_heatmap <- function(metrics_df, metric) {
+  # Ensure the metric column is treated as numeric
+  metrics_df[[metric]] <- as.numeric(metrics_df[[metric]])
+  
+  # Update labels for readability
+  metrics_df$treatment <- factor(metrics_df$treatment,
+                                 levels = c("original", "original_boxed", "segment", "segment_bodyPart"),
+                                 labels = c("original", "original\nboxed", "segment", "segmented\nbody part"))
+  
+  metrics_df$assess_area <- factor(metrics_df$assess_area,
+                                   levels = c("hindleg cleanliness", "hindquarter cleanliness", "udder cleanliness"),
+                                   labels = c("hindleg", "hindquarter", "udder"))
+  
+  # Plot with updated aesthetics
+  plot <- ggplot(metrics_df, aes(x=assess_area, y=treatment, fill=get(metric))) +
+    geom_tile(color = "white", size = 0.5) +  # Adding white borders for clearer separation
+    geom_text(aes(label = sprintf("%.2f", get(metric))), vjust = 0.5, hjust = 0.5, color = "white", size = 6) +  # Center values in each cell
+    scale_fill_gradient(low = "lavender", high = "mediumorchid3", guide = "none") +  # Apply new color gradient
+    labs(title = paste("Heatmap of", metric), x = "Assessment Area", y = "Treatment") +
+    theme_classic(base_size = 25) +  # Apply classic theme with larger base text size
+    theme(
+      axis.title = element_text(size = 28),  # Larger axis titles
+      plot.title = element_text(size = 30)   # Larger plot title
+    )
+  
+  return(plot)
 }
--- src/cleanliness_precision_recall.R
+++ src/cleanliness_precision_recall.R
@@ -1,4 +1,6 @@
-library(dplyr)
+#library(dplyr)
+library(ggplot2)
+library(viridis)
 
 setwd("C:/Users/skysheng/Desktop/lameness_GPT4V/results_welfare_assess/cleanliness")
 results <- read.csv("welfare_assess_cleanliness_gpt4o.csv", header = TRUE)
@@ -7,58 +9,29 @@ results <- read.csv("welfare_assess_cleanliness_gpt4o.csv", header = TRUE)
 results$true_score <- as.numeric(results$true_score)
 results$predict_score <- as.numeric(results$predict_score)
 
-############################ percentage of predicted score ############################
+############################ percentage of accurate prediction ############################
 # as large language models output are probability distribution, we randomly run the 
 # same test image multiple times to get GPT-4o's assessment results. 
 # we calculate the frequency of each image being predicted as score 0 and 2
+# for each image, we get an accuracy measure, that reprecent percentage of times
+# GPT-4o made the right prediction
 hindleg <- results[which(results$assess_area == "hindleg cleanliness"),]
-summary_df <- summarize_scores(hindleg)
+summary_df <- summarize_scores(results)
+summary_df$inaccurate_pct <- 1-summary_df$accurate_predict_pct
+overall_accuracy <- mean(summary_df$accurate_predict_pct)
+metrics_list <- weighted_precision_recall(summary_df)
+overall_precision <- metrics_list$precision
+overall_recall <- metrics_list$recall
 
 
+##################### accuracy, precision, recall for each group###########################
+all_metrics_df <- calculate_metrics(results)
+# Plot heatmaps for each metric
+accuracy_heatmap <- plot_heatmap(all_metrics_df, "accuracy")
+precision_heatmap <- plot_heatmap(all_metrics_df, "precision")
+recall_heatmap <- plot_heatmap(all_metrics_df, "recall")
 
-############################ overall precision & recall ############################
-original <- results[which(results$treatment == "original"),]
-# Calculate True Positives (TP), False Positives (FP), and False Negatives (FN)
-TP <- sum(original$predict_score == 2 & original$true_score == 2)
-FP <- sum(original$predict_score == 2 & original$true_score == 0)
-FN <- sum(original$predict_score == 0 & original$true_score == 2)
-
-# Calculate Precision and Recall
-precision <- TP / (TP + FP)
-recall <- TP / (TP + FN)
-
-# Print Precision and Recall
-cat("Overall Precision:", precision, "\n")
-cat("Overall Recall:", recall, "\n")
-
-
-############################ hindleg precision & recall ############################
-hindleg <- original[which(original$assess_area == "hindleg cleanliness"),]
-# Calculate True Positives (TP), False Positives (FP), and False Negatives (FN)
-TP <- sum(hindleg$predict_score == 2 & hindleg$true_score == 2)
-FP <- sum(hindleg$predict_score == 2 & hindleg$true_score == 0)
-FN <- sum(hindleg$predict_score == 0 & hindleg$true_score == 2)
-
-# Calculate Precision and Recall
-precision <- TP / (TP + FP)
-recall <- TP / (TP + FN)
-
-# Print Precision and Recall
-cat("Hindleg Precision:", precision, "\n")
-cat("Hindleg Recall:", recall, "\n")
-
-
-############################ hindleg & hindquarter precision & recall ############################
-hindleg_hindquarter <- original[which((original$assess_area == "hindleg cleanliness") | (original$assess_area == "hindquarter cleanliness") ),]
-# Calculate True Positives (TP), False Positives (FP), and False Negatives (FN)
-TP <- sum(hindleg_hindquarter$predict_score == 2 & hindleg_hindquarter$true_score == 2)
-FP <- sum(hindleg_hindquarter$predict_score == 2 & hindleg_hindquarter$true_score == 0)
-FN <- sum(hindleg_hindquarter$predict_score == 0 & hindleg_hindquarter$true_score == 2)
-
-# Calculate Precision and Recall
-precision <- TP / (TP + FP)
-recall <- TP / (TP + FN)
-
-# Print Precision and Recall
-cat("Hindleg & hindquarter Precision:", precision, "\n")
-cat("Hindleg & hindquarter Recall:", recall, "\n")
+# Print or save the plots
+print(accuracy_heatmap)
+print(precision_heatmap)
+print(recall_heatmap)
